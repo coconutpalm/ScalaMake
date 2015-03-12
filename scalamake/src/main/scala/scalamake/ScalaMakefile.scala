b6f10768e4398756ff5c4b179e3d80944feeefe4
David Orme <djo@coconut-palm-software.com>
2015-03-12 15:01:27 -0500

    Fixed warning

--- scalamake/src/main/scala/scalamake/ScalaMakefile.scala
+++ scalamake/src/main/scala/scalamake/ScalaMakefile.scala
@@ -3,7 +3,7 @@ package scalamake
 import Implicits._
 
 trait ScalaMakefile {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]) : Unit = {
     if (args.length >= 1) {
       scalaMake(args(0))
     } else {
